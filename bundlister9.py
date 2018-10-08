72355c64de5c6ff1c1935cf2ffb5d6b3ebbd5754
michaelvincerra <michael.vincerra@gmail.com>
2018-10-07 19:49:21 -0700

    Modifies template variables and adds combinator() to bundlister9.py

--- bundlister9.py
+++ bundlister9.py
@@ -2,14 +2,13 @@ import os, re, urllib
 import jinja2
 from jinja2 import Environment, FileSystemLoader, Template
 import git
-from bunpun.bundle_pundle import BunPun
+# from collections import defaultdict
 
 
 # Next steps: 
-#   Combine pundles array with the bundles array
-#   Sort all combined items alphabetically.
 #   Write if/else condition in the template logic to show bundle and/or pundle    
 #   Revise directory structure relative to Makefile in bundle_lister() function. 
+#   Write .sh script to call Python script and delete cloned_repo
 
 GITHUB_BASE="https://github.com/clearlinux/clr-bundles/tree/master/bundles/"
 PUNDLES="https://github.com/clearlinux/clr-bundles/blob/master/packages"
@@ -57,103 +56,26 @@ def pundler():
             pundle = PATTERN4.findall(i)
 
             if pundle: 
-                pundle_title = pundle[0]
+                pundle_title = pundle[0].strip()
                 pundle_list.append(pundle_title)
 
             purl = PUNDLES
 
     return {"pundle_list": pundle_list, "purl": purl }
 
-# def baker(lines): 
-#     """
-#     Output an alphabetized dictionary to be called by its key in template.html
-#     """
-#     bundle_title = "bundle_title"
-#     pundle_title = "pundle_title"
-#     combo_title = []
-
-#     combo_title.append(pundler())
-
-#     for i in lines: 
-#         combo_title.append(extractor(lines))
-    # print(combo_title)
-
-    # if (dict.get('key') is not None) and ('key' in dict):
-
-    # for b in combo_title:
-    #     if 'data_title' is not None and 'data_title' in combo_title:
-    #         bundle_title = b.get('data_title')
-    #         combo_title.append(bundle_title)
-    # #         bundle_title = b.data_title.values() # See error message on the string here. 
-    # #         combo_title.append(bundle_title)
-
-    # for p in combo_title:
-    #     if 'pundle_list' is not None and 'pundle_list' in combo_title:
-    #         pundle_title = p.get('pundle_list')
-    #         combo_title.append(pundle_title)
-
-    # for p in combo_title:
-    #     if 'pundle_list':
-    #         pundle_title = p.get('pundle_list')    
-    
-    # puns = list(pundle_title)
-    
-    # for pun in puns:
-    #     combo_title.append(pun) 
-
-    # combo_title.sort()
-
-
-    # baked = bakery.sort()
-    # combo_title = pundle_title + bundle_title
-
-# #     bread = baking.sort()
-# # #   REMOVE NONE !!!! baking = list(filter(None, bakery))
-# #     # bread = dict(enumerate(buns,start=1))
-    print(combo_title)
-    
-    return {"combo_title": combo_title}
 
 def bundler():
     git.Git("/Users/michaelevan/temp/intel_python/rattlesnake/cloned_repo/").clone("https://github.com/clearlinux/clr-bundles.git")
     data = []
-    bakery = []
-    bundle_title = "bundle_title"
-    pundle_title = "pundle_title"
+    # bundle_only = []
+
 
     for root, dirs, files in os.walk("/Users/michaelevan/temp/intel_python/rattlesnake/cloned_repo/clr-bundles/bundles", topdown=False):
         for name in files:
             with open(os.path.join(root, name)) as file_obj:
                 lines = file_obj.readlines()
                 data.append(extractor(lines))
-
     data.append(pundler())
-    # data.append(baker(lines))
-    # print(data)
-    for d in data:
-        if 'data_title' is not None and 'data_title' in data:
-            bundle_title = str(d.get('data_title'))
-            bakery.append(bundle_title)
-
-    for p in data:
-        if 'pundle_list' is not None and 'pundle_list' in data:
-            pundle_title = p.get('pundle_list')    
-    
-    puns = list(pundle_title)
-    for pun in puns:
-        bakery.append(pun) 
-
-    # alphabaked = bakery.sort()
-    # data.append(baker())
-#   REMOVE NONE from for loop !!!! baking = list(filter(None, bakery))
-    
-    # b = {BunPun(k) : v for k, v in baked.items()}
-    # print(b)
-    # baked = dict(enumerate(bakery,start=1))
-    # bread = baked.items()
-    # dozens = {'bread': bread} 
-    # data.append(dozens)
-    print(bakery)
     print(data)
  
     loader = jinja2.FileSystemLoader(searchpath='./') 
@@ -163,6 +85,38 @@ def bundler():
     with open('bundles01.html', 'w') as file:
         file.write(output)
 
+
+def combinator(): 
+    """
+    Output an alphabetized dictionary to be called by key in template.html
+    """
+    combo_title = []
+    bundle_title = "bundle_title"
+    pundle_title = "pundle_title"
+
+    combo_title.append(bundler())
+
+    for d in data:
+        if 'data_title':
+            bundle_title = str(d.get('data_title'))
+            combo_title.append(bundle_title)
+
+    for p in data:
+        if 'pundle_list':
+    #     # if 'pundle_list' is not None and 'pundle_list' in data:            
+            pundle_title = p.get('pundle_list') 
+ 
+    puns = list(pundle_title)
+    for pun in puns:
+        combo_title.append(pun) 
+
+    combo_title.sort()
+    
+    return {"combo_title": combo_title}
+
+
+
 bundler()
+
 
 

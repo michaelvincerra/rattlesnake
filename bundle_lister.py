328b24466e5572e627065d3a8b8765463b1bc68c
michaelvincerra <michael.vincerra@gmail.com>
2018-11-03 17:37:31 -0700

    Revises sortedData to filter based on lower() for readability.

--- bundle_lister.py
+++ bundle_lister.py
@@ -36,7 +36,6 @@ def extractor(lines):
         if includes:
             include_text = includes[0].strip("()")
             include_list.append(include_text)
-
     return {"title": bundle_title, "data_desc": data_desc, "include_list": include_list, "url": url}
 
 def pundler():
@@ -62,12 +61,11 @@ def pundler():
 
         for pun, desc in zip(pundle_list, pun_desc): 
                 pundle_master.append({"title": pun, "pun_desc": desc, "purl": PUNDLES})
-
     return pundle_master
-
 
 def bundler():
     data = []
+
     try:
         git.Git("./cloned_repo/").clone("https://github.com/clearlinux/clr-bundles.git")
     except:
@@ -81,12 +79,14 @@ def bundler():
     pundle_master = pundler()
     data = data + pundle_master
     filtered = list(filter(lambda x: x.get('title'), data))
-    sortedData = sorted(filtered, key=itemgetter('title'))
+    sortedData = sorted(filtered, key=lambda x:x['title'].lower()) 
+    #ALT sortedData2 = sorted(sortedData, key=itemgetter('title'))
     loader = jinja2.FileSystemLoader(searchpath='./')
     env = jinja2.Environment(loader=loader)
     template = env.get_template('template.html')
+    
     output = template.render(data=sortedData)
     with open('bundles.html', 'w') as file:
-        file.write(output)
-        
+        file.write(output)   
+          
 bundler()

01645a3b162abe2db300dfebd2e2210a32611fbf
michaelvincerra <michael.vincerra@gmail.com>
2019-08-28 15:04:41 -0700

    Adds PATTERN4 regex, code to capture status, and template revisions to filter display of tags.

--- bundle_lister.py
+++ bundle_lister.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import io
 import os
 import re
@@ -9,16 +11,11 @@ from operator import itemgetter
 from datetime import datetime 
 
 GITHUB_BASE = "https://github.com/clearlinux/clr-bundles/tree/master/bundles/"
-PUNDLES = "https://github.com/clearlinux/clr-bundles/blob/master/packages"
 
-PATTERN1 = re.compile(r"#\s?\[TITLE]:\w?(.*)")
-PATTERN2 = re.compile(r"#\s?\[DESCRIPTION]:\w?(.*)")
-PATTERN3 = re.compile(r"\(([^()]*|include)\)", re.MULTILINE)
-PATTERN4 = re.compile(r"^((?:(?!#)\w+[^-\s][-])\w+|\w+[^\s-])", re.MULTILINE)
-# ALT PATTERN4 = re.compile(r"^((?:(?!#)(\w+[^-\s])[-]\w+.)[^\s]{1,}[^\s]|\w+[^\s-])", re.MULTILINE)
-PATTERN5 = re.compile(r"^\w.+\s[#]\s(\w?.*)?", re.MULTILINE)
-# PATTERN5 = re.compile(r"^(?!=a)\w.+\s[#]\s(\w?.*)?", re.MULTILINE)
-# Previous version: PATTERN5 = re.compile(r"^[^#].*(?<=\s\-\s)(\w+.*)?", re.MULTILINE)
+PATTERN1 = re.compile(r"#\s?\[TITLE]:\s?(.*)")
+PATTERN2 = re.compile(r"#\s?\[DESCRIPTION]:\s?(.*)")
+PATTERN3 = re.compile(r"(?<=include)\(.*\)", re.MULTILINE)
+PATTERN4 = re.compile(r"(#\s?\[STATUS]):\s?(.*)")
 
 def extractor(lines):
     bundle_title = "title"
@@ -26,11 +23,13 @@ def extractor(lines):
     url = "url"
     include_list = []
     include_unique = []
+    status = "status"
 
     for i in lines:
         title = PATTERN1.match(i)
         desc = PATTERN2.match(i)
         includes = PATTERN3.findall(i)
+        curr_status = PATTERN4.match(i)
 
         if title:
             bundle_title = title.groups(0)[0].strip()
@@ -38,40 +37,16 @@ def extractor(lines):
             data_desc = desc.groups(0)[0].strip()
         if url:
             url = os.path.join(GITHUB_BASE, bundle_title)
+        if curr_status:
+            status = curr_status.group(2)
 
         if includes:
             include_text = includes[0].strip("()")
             include_list.append(include_text)
             include_unique = set(include_list)
-    return {"title": bundle_title, "data_desc": data_desc, "include_list": include_unique, "url": url}
+    return {"title": bundle_title, "data_desc": data_desc, "include_list": include_unique, "url": url, "status": status}
 
-def pundler():
-    with io.open("./cloned_repo/clr-bundles/packages") as file_obj:
-        lines = file_obj.readlines()
-        pundle_title = "pundle_title"
-        pundle_desc = "pundle_desc"
-        purl = "purl" 
-        pundle_list = []
-        pun_desc = []
-        pundle_master = []
-        
-        for i in lines:
-            pundle = PATTERN4.findall(i)
-            pundle_plus = PATTERN5.findall(i)
-            
-            if pundle:
-                pundle_title = pundle[0]
-                pundle_list.append(pundle_title)
-
-            if pundle_plus:
-                pundle_desc = pundle_plus[0].strip("[]")
-                pun_desc.append(pundle_desc)
-
-        for pun, desc in zip(pundle_list, pun_desc): 
-                pundle_master.append({"title": pun, "pun_desc": desc, "purl": PUNDLES})
-    return pundle_master
-
-def bundler():
+def main():
     data = []
     try:
         git.Git("./cloned_repo/").clone("https://github.com/clearlinux/clr-bundles.git")
@@ -82,10 +57,8 @@ def bundler():
             with open(os.path.join(root, name)) as file_obj:
                 lines = file_obj.readlines()
                 data.append(extractor(lines))
-
-    pundle_master = pundler()
-    data = data + pundle_master 
     filtered = list(filter(lambda x: x.get('title'), data))
+
     sortedData = sorted(filtered, key=lambda x:x['title'].lower())
     #ALT sortedData2 = sorted(sortedData, key=itemgetter('title'))
     loader = jinja2.FileSystemLoader(searchpath='./')
@@ -94,7 +67,9 @@ def bundler():
     template.globals['now'] = datetime.utcnow
 
     output = template.render(data=sortedData, now=datetime.utcnow())
-    with io.open('bundles.html.txt', 'w') as file:
+    with io.open('bundles.html', 'w') as file:
         file.write(output)   
           
-bundler()
+if __name__ == "__main__":
+    main()
+    
